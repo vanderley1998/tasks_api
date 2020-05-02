aef5403a83837079c3bb286cf3ed2f53684f7f20
Vanderley Junior <vanderley_199@hotmail.com.br>
2020-05-02 15:20:21 -0300

    fix: folders structure

--- LubyTask.Domain/Entities/Classes/CredentialUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LubyTasks.Domain.Utils.Classes
-{
-    public class CredentialUser
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
--- LubyTasks.API/Startup.cs
+++ LubyTasks.API/Startup.cs
@@ -1,12 +1,17 @@
+using LubyTasks.API.Filters;
+using LubyTasks.Domain;
+using LubyTasks.Domain.Utils;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 
-namespace LubyTasks
+namespace LubyTasks.IdentyServer
 {
     public class Startup
     {
@@ -21,7 +26,23 @@ namespace LubyTasks
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContext<LubyTasksDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<LubyTasksHandler>();
+            services.AddScoped<CurrentUser>();
+            services.AddScoped<StatusRequestFilter>();
+            services.AddScoped<CurrentUserFilter>();
+            services.AddServerSideBlazor(o => o.DetailedErrors = true);
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                });
+
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = "JwtBearer";
@@ -35,25 +56,75 @@ namespace LubyTasks
                     ValidateAudience = false,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("WmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/")),
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Key)),
                     ClockSkew = TimeSpan.FromMinutes(5)
                 };
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.CustomSchemaIds(x => x.ToString());
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Luby Tasks API",
+                    Description = "A test using ASP.NET Core 3.1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vanderley Sousa",
+                        Email = "vanderley_1998@hotmail.com.br",
+                        Url = new Uri("https://github.com/vanderley1998"),
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "You can get the token in \"/auth\""
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+                c.EnableAnnotations();
             });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+            Authentication.KeyJwt = Key;
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Luby Tasks API");
+                options.RoutePrefix = string.Empty;
+            });
+
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
